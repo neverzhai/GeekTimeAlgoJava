a5293786be4633b466390cd71ecd865a99a4de5b
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2021-04-07 11:33:37 +0800

    max product

---
+++ src/main/java/geektime/algo/leecode/MaxProduct.java
@@ -0,0 +1,47 @@
+package geektime.algo.leecode;
+
+/**
+ * @author: zhaixiaoshuang
+ * @date: 2021-04-07 10:17
+ * @description: 乘机最大子数组
+ */
+public class MaxProduct {
+
+    public static void main(String[] args) {
+         int[] nums = {2,3,-2,4};
+         int result = maxProduct(nums);
+    }
+
+
+    // method1 : 动态规划, 不考虑空间优化
+    private static int maxProduct(int[] nums) {
+        int length = nums.length;
+        int[] maxF = new int[length];
+        int[] minF = new int[length];
+        System.arraycopy(nums, 0, maxF, 0, length);
+        System.arraycopy(nums, 0, minF, 0, length);
+        for (int i = 1; i < length; ++i) {
+            maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
+            minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+        }
+        int ans = maxF[0];
+        for (int i = 1; i < length; ++i) {
+            ans = Math.max(ans, maxF[i]);
+        }
+        return ans;
+    }
+
+
+    // method2: 动态规划, 考虑空间优化
+    private static int maxProduct1(int[] nums) {
+        int maxF = nums[0], minF = nums[0], ans = nums[0];
+        int length = nums.length;
+        for (int i = 1; i < length; ++i) {
+            int mx = maxF, mn = minF;
+            maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
+            minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
+            ans = Math.max(maxF, ans);
+        }
+        return ans;
+    }
+}
