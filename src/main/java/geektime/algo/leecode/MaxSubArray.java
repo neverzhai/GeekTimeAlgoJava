2bf63cb55ccfebfb5bcf683c1b3cbde116c7ea60
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2021-04-11 15:21:59 +0800

    53: max sub array

--- src/main/java/geektime/algo/leecode/MaxSubArray.java
+++ src/main/java/geektime/algo/leecode/MaxSubArray.java
@@ -6,4 +6,33 @@ package geektime.algo.leecode;
  * @description: 53. 最大子序和
  */
 public class MaxSubArray {
+
+
+    // 动态规划, 状态转移方程: f(i)=max{f(i−1)+nums[i],nums[i]}
+
+    // 未空间优化
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i -1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+
+    // 空间优化
+    public int maxSubArray1(int[] nums) {
+        int max = nums[0];
+        int pre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            max = Math.max(max, pre);
+        }
+
+        return max;
+
+    }
 }
