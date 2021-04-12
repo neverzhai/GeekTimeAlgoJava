0443eb537951dd63968a59776a163919c8c94f89
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2021-04-12 10:24:34 +0800

    add permute

--- src/main/java/geektime/algo/leecode/top100Medium/Permute.java
+++ src/main/java/geektime/algo/leecode/top100Medium/Permute.java
@@ -12,6 +12,8 @@ import java.util.List;
  */
 public class Permute {
 
+
+    // 回溯法
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
 
@@ -38,6 +40,34 @@ public class Permute {
             // 撤销操作
             Collections.swap(output, first, i);
         }
+    }
+
+    // 回溯法
+    List<List<Integer>> list;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        list=new ArrayList<List<Integer>>();//最终的结果
+        List<Integer> team=new ArrayList<Integer>();//回溯过程收集元素
+        boolean jud[]=new boolean[nums.length];//用来标记
+        dfs(jud, nums, team, 0);
+        return list;
+    }
+    private  void dfs(boolean[] jud, int[] nums, List<Integer> team, int index) {
+        int len = nums.length;
+        if (index == len)// 停止
+        {
+            list.add(new ArrayList<Integer>(team));
+        } else {
+            for (int i = 0; i < len; i++) {
+                if (jud[i]) //当前数字被用过 当前即不可用
+                    continue;
+                team.add(nums[i]);
+                jud[i]=true;//标记该元素被使用
+                dfs(jud, nums, team, index + 1);
+                jud[i] = false;// 还原
+                team.remove(index);//将结果移除临时集合
+            }
+        }
+
     }
 
 }
