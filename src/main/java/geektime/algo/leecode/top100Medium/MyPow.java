6c3899a8b3cc65f35cf4eb6324e7aee86931baba
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2021-04-15 14:36:51 +0800

    add my pow

--- src/main/java/geektime/algo/leecode/top100Medium/MyPow.java
+++ src/main/java/geektime/algo/leecode/top100Medium/MyPow.java
@@ -9,12 +9,41 @@ public class MyPow {
 
     // method1 -- 分治 + 递归
     public double myPow(double x, int n) {
-        return x;
+        long N = n;
+
+        return N >=0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    private double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+
+        return N % 2 == 0 ? y*y : y*y*x;
     }
 
 
     // method2 -- 分治 + 迭代
     public double myPow1(double x, int n) {
-        return x;
+        long N = n;
+
+        return N >=0 ? quickMul1(x, N) : 1.0 / quickMul1(x, -N);
+    }
+
+    private double quickMul1(double x, long N) {
+        double ans = 1.0;
+
+        double x_contribute = x;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= x_contribute;
+            }
+
+            x_contribute *= x_contribute;
+            N /=2;
+        }
+        return ans;
     }
 }
